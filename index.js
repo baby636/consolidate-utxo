ec89225c80b89d6a07474c6231c9d6f00e79a123
U-Zyn Chua <chua@uzyn.com>
2020-11-17 12:19:10 +0800

    Doc update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const HELP = `Consolidates UTXO on your bitcoin node.
   --port     Port where bitcoin json-rpc is listerning. Default is "8332".
   --limit    Limit number of inputs.
   --amount   Maximum amount for UTXO to include in transaction. Default is "0.0001".
-  --output   Output address.
+  --output   Manually specifying output address. (Optional: leaving it blank would generate a new address every run)
   --fee      Fee for new transaction in Satoshi/byte. Default is "1".
 
   Designed for Bitcoin version >= 0.17
